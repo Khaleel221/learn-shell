7c9c903c0190b391a7d3985498dfd16bd4901edb
Khaleel <khaleelshaik711@gmail.com>
2025-03-17 10:04:36 +0530

    Adding colors to script

---
+++ Shell_colors.sh
@@ -0,0 +1,18 @@
+# echo -e \e[colm message\e[0m
+#\e -> to enable colors
+#colm -> color code
+#\e[om - to disable colors
+#color code
+#red(31)
+#yellow(32)
+#green(33)
+#blue(34)
+#magenta(35)
+#cyan (35)
+
+echo -e "\e[31m Hello world!\e[0m"
+echo -e "\e[32m Hello world!\e[0m"
+echo -e "\e[33m Hello world!\e[0m"
+echo -e "\e[34m Hello world!\e[0m"
+echo -e "\e[35m Hello world!\e[0m"
+echo -e "\e[36m Hello world!\e[0m"
