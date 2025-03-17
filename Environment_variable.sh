63fbf01902233c26b15be54062b270d00b92bbb6
Khaleel <khaleelshaik711@gmail.com>
2025-03-17 10:41:56 +0530

    environment variable

--- Environment_variable.sh
+++ Environment_variable.sh
@@ -1,14 +1,14 @@
 # environment variable is a global variable it can used any where in script
 #printenv - to list the all environment variable
 
-echo "User Home directory: $Home"
-echo "User :$user"
-echo "print working directory: $pwd"
-echo "path to current shell: $shell"
-echo "list of directories to search for executable: $path"
+echo "User Home directory: $HOME"
+echo "User :$USER"
+echo "print working directory: $PWD"
+echo "path to current shell: $SHELL"
+echo "list of directories to search for executable: $PATH"
 
 #to add environment variable
 
-export testing="test"
+export TESTING="test"
 
 echo $testing
