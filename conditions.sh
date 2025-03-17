cc958632c79954e007301eba5a2fb8a0b6efa62b
Khaleel <khaleelshaik711@gmail.com>
2025-03-17 16:34:06 +0530

    functions

--- conditions.sh
+++ conditions.sh
@@ -1,7 +1,16 @@
-number = 10
+#number = 10
+#
+#if [$number -gt 15]; then
+#  echo "greater than 10"
+#else
+#  echo "less than 10"
+#fi
 
-if [$number -gt 15]; then
-  echo "greater than 10"
-else
-  echo "less than 10"
-fi
+marks=75
+if [ $marks -ge 90 ]; then
+ echo "topper"
+ elif [ $marks -ge 60 ]; then
+ echo "first class"
+ else
+   echo "second class"
+   fi
