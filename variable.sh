b701a1ab2e32e7a357377b5b7e33ec4c64b4a993
Khaleel <khaleelshaik711@gmail.com>
2025-03-17 07:00:52 +0530

    my fisrt variable example

---
+++ variable.sh
@@ -0,0 +1,12 @@
+#if we assign a name to set of data then its called a variable
+# In Shell scripting, there are no specific data types.
+# All variables are treated as strings by default.
+
+#declaring a variable
+variable_name="khaleel"
+age=24
+
+#accessing a variable
+
+echo name: $variable_name
+echo age: $age
