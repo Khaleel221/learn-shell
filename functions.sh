ec9df3a02817282fc21501fd420954f3951f9c6c
Khaleel <khaleelshaik711@gmail.com>
2025-03-17 15:53:11 +0530

    functions

--- functions.sh
+++ functions.sh
@@ -1,18 +1,17 @@
 #in shell scripting functions allows you to group of commands into reusable blocks of code. helpful to organize the code struture and to reuse the code
 
 #basic function
-fucntion_name{
+fucntion_name(){
   echo "Hello World!"
 }
 
 fucntion_name #calling function
 
 #function with arguments
-test(){
+testr(){
   echo "hello $1 and $2"
 }
-
-test "khaleel" "shaik"
+testr "khaleel" ,"shaik"
 
 
 
