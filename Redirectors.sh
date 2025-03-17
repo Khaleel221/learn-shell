8ab6ba5067f9323a0213f37095552b84e490867c
Khaleel <khaleelshaik711@gmail.com>
2025-03-17 12:05:28 +0530

    redirectors

--- Redirectors.sh
+++ Redirectors.sh
@@ -1,3 +1,7 @@
 #redirectors used to control the input and output of commands. it allows to redirect standard input and standard output and error messgae
 
-ls > output.txt
+#redirect the output , overwrite if file exists
+#ls > output.txt
+
+#redirect the output , append if file exists
+ls >> output.txt
