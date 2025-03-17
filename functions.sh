b927d57230ab333ad00dd4d7c8609381e24b22a6
Khaleel <khaleelshaik711@gmail.com>
2025-03-17 15:39:46 +0530

    redirectors

---
+++ functions.sh
@@ -0,0 +1,6 @@
+#in shell scripting functions allows you to group of commands into reusable blocks of code. helpful to organize the code struture and to reuse the code
+
+function_name(){
+  echo "Hello World"
+}
+function_name
