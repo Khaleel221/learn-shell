aad4516a85f1dff21b3bc5ff119b927cb83237b4
Khaleel <khaleelshaik711@gmail.com>
2025-03-18 16:24:49 +0530

    even or odd checking

---
+++ even_or_odd.sh
@@ -0,0 +1,19 @@
+#check given number is even or odd
+
+if [ $# -ne 1 ];then
+  echo "usage $0 <argument>"
+  example "$0 <argument>"
+  exit 1
+  fi
+
+number=$1
+
+
+
+if [ $((number % 2)) -eq 0];then
+  echo "number is even"
+  elseif [ $number =~ ^-[0-9]$ ];then
+    echo "number is invalid"
+  else
+    echo "number is odd"
+fi
