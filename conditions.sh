4a85281727052ffcc091e459fd49de50e1a7f5f6
Khaleel <khaleelshaik711@gmail.com>
2025-03-18 12:03:03 +0530

    script that prints the current logged-in user and the system uptime.

--- conditions.sh
+++ conditions.sh
@@ -5,13 +5,26 @@
 #else
 #  echo "less than 10"
 #fi
-echo enter your marks
-read marks
-if [ $marks -ge 90 ]; then
- echo "topper"
- elif [ $marks -ge 60 ]; then
- echo "first class"
- else
-   echo "second class"
-   fi
+#echo enter your marks
+#read marks
+#if [ $marks -ge 90 ]; then
+# echo "topper"
+# elif [ $marks -ge 60 ]; then
+# echo "first class"
+# else
+#   echo "second class"
+#   fi
 
+echo "enter a following color (red, green, blue)"
+read color
+case $color in
+red) echo "your color is red"
+;;
+blue) echo "your color is blue"
+;;
+green) echo "your color is green"
+;;
+*)
+ echo "invalid"
+;;
+esac
