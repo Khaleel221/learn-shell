3acfe0ba8b5c9d6e86af8e262edc980781ec3b41
Khaleel <khaleelshaik711@gmail.com>
2025-03-18 15:35:49 +0530

    case statement

--- calculator.sh
+++ calculator.sh
@@ -1,6 +1,6 @@
 #Write a script that takes two numbers and an operator (+, , , /) as input and performs the calculation.
 
-if [$# -ne 3];then
+if [ $# -ne 3 ];then
  echo "$? <number1> <opreator> <number>"
  echo "example $? 2 + 3"
  exit 1
@@ -10,14 +10,14 @@ num1=$1
 operator=$2
 num2=$3
 
-case $operator in
+case "$operator" in
 +)
 result=$(($num1 + $num2))
-  ;;
+;;
 -)
 result=$(($num1 - $num2))
 ;;
-*)
+\*)
 result=$(($num1 * $num2))
 ;;
 /)
@@ -28,6 +28,7 @@ if [$num2 -eq 0];then
 result=$(($num1 / $num2))
 *)
 echo "invalid number"
+exit 1
 ;;
 esac
 
