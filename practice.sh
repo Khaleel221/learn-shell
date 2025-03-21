e3d79178a5612e8ff4ba973dbff101142136f6ad
Khaleel <khaleelshaik711@gmail.com>
2025-03-21 15:14:03 +0530

    even or odd checking

--- practice.sh
+++ practice.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-# Write a script that takes a filename as an argument and prints its contents.
+## Write a script that takes a filename as an argument and prints its contents.
+#
+##check argument provided or not
+#if [ $# -eq 0 ];then
+#  echo"usage:$0 <filename>"
+#  exit 1
+#fi
+#
+#filename=$1
+#
+#if [ ! -f "$filename" ]; then
+#  echo "filename not found"
+#  exit 1
+#fi
+#
+#echo  "content of filename $filename:"
+#cat "$filename"
 
-#check argument provided or not
-if [ $# -eq 0 ];then
-  echo"usage:$0 <filename>"
-  exit 1
-fi
+#Create a script that prints the current logged-in user and the system uptime.
 
-filename=$1
-
-if [ ! -f "$filename" ]; then
-  echo "filename not found"
-  exit 1
-fi
-
-echo  "content of filename $filename:"
-cat "$filename"
-
+echo "user name is $USER"
+echo "uptime is $(uptime)"
