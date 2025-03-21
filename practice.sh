81cb4dfefdbfb4c0b0f0498dd8953814aa6a0edd
Khaleel <khaleelshaik711@gmail.com>
2025-03-21 15:23:12 +0530

    even or odd checking

--- practice.sh
+++ practice.sh
@@ -18,7 +18,41 @@
 #echo  "content of filename $filename:"
 #cat "$filename"
 
-#Create a script that prints the current logged-in user and the system uptime.
+##Create a script that prints the current logged-in user and the system uptime.
+#
+#echo "user name is $USER"
+#echo "uptime is $(uptime)"
 
-echo "user name is $USER"
-echo "uptime is $(uptime)"
+#Write a script that takes two numbers and an operator (+, , , /) as input and performs the calculation.
+
+if [ $# -ne 3 ]; then
+  echo "usage is:$0 <num1> <operator> <num2>"
+  exit 1
+fi
+
+num1=$1
+operator=$2
+num2=$3
+
+case $operator in
++)
+result=$(($num1 + $num2))
+;;
+-)
+result=$(($num1 - $num2))
+;;
+/*)
+result=$(($num1 * $num2))
+;;
+/)
+ if [ $num2 -eq 0 ];then
+   echo "not dividable"
+   exit 1
+   fi
+  result=$(($num1 / $num2))
+;;
+*)
+echo "enter valid number"
+esac
+
+echo "$result"
