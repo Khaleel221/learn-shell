fd4c9309547dfa8b2bc2d7c3406fb9cbda0c6988
Khaleel <khaleelshaik711@gmail.com>
2025-10-01 06:22:45 +0530

    colours in shel

--- colours.sh
+++ colours.sh
@@ -1,12 +1,18 @@
+#syntax
 
-touch text1
-echo -e "\e[31mchecking list of files\e[0m"
-ls  text1
-echo -e "\e[32mcreating directory\e[0m"
-mkdir demo
-echo -e "\e[33mcreating text file\e[0m"
-touch text
-echo -e "\e[34mremoving directory\e[0m"
-rm -rf demo
-echo -e "\e[35m removing text file\e[0m"
-rm -f text
+# echo -e "\e[31m message\e[0m"
+
+#to print red colour
+echo -e "\e[31m printing red colour\e[0m"
+
+#to print green colour
+echo -e "\e[32m printing green colour\e[0m"
+
+#to print yellow colour
+echo -e "\e[33m printing yellow colour\e[0m"
+
+#for bold and underline with blue
+echo -e "\e[1;4;34m printing blue colour\e[0m"
+
+#with info
+echo -e "\e[1;4;34m[INFO] printing blue colour\e[0m"
