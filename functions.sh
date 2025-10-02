91f850a936e2f639f01032d484dbcdfa0e0c532d
Khaleel <khaleelshaik711@gmail.com>
2025-10-02 21:38:29 +0530

    functions in shell script

--- functions.sh
+++ functions.sh
@@ -1,10 +1,17 @@
 #function in shell scripting reusable blocks of code , define once and execute multiple times with optional arguments and return values
 
-#1.simple function problens
+##1.simple function problens
+#
+##define function
+#hello(){
+#  echo "Hello world"
+#}
+##calling function
+#hello
 
-#define function
+#2.functions with arguments
+
 hello(){
-  echo "Hello world"
+  echo "argument 1 is $1 and argument two is $2"
 }
-#calling function
-hello
+hello "khaleel" "shaik"
