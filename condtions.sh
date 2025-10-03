c610bab4add249bc0a19b93c14927aed10cd9911
Khaleel <khaleelshaik711@gmail.com>
2025-10-03 07:26:01 +0530

    conditions in shell scripting

--- condtions.sh
+++ condtions.sh
@@ -18,12 +18,22 @@
 #  echo "$file_path does not exits"
 #fi
 
-#Compare Two Strings
-read -p "enter string 1: " str1
-read -p "enter string 2: " str2
+##Compare Two Strings
+#read -p "enter string 1: " str1
+#read -p "enter string 2: " str2
+#
+#if [ $str1 == $str2 ];then
+# echo "$str1 and $str2 are same"
+#else
+#  echo "both are diffrent"
+#fi
 
-if [ $str1 == $str2 ];then
- echo "$str1 and $str2 are same"
+#Check User Login
+
+read -p "enter user name" user
+
+if whoami | grep -q $user ; then
+  echo "$user logged in"
 else
-  echo "both are diffrent"
+  echo "$user not logged in"
 fi
