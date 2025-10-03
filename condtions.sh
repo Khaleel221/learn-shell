c366d59f853da99ff4c375d3674809cfe6de3ffe
Khaleel <khaleelshaik711@gmail.com>
2025-10-03 06:57:31 +0530

    conditions in shell scripting

--- condtions.sh
+++ condtions.sh
@@ -1,13 +1,19 @@
 #1.2Check if a Number is Positive or Negative
 
-read -p "enter a number: " a
-if [ $a -gt 0 ];then
-  echo "$a is postive number"
-elif [ $a -lt 0 ];then
-  echo "$a is Negative number"
-else
-  echo "The $a is zero"
-fi
+#read -p "enter a number: " a
+#if [ $a -gt 0 ];then
+#  echo "$a is postive number"
+#elif [ $a -lt 0 ];then
+#  echo "$a is Negative number"
+#else
+#  echo "The $a is zero"
+#fi
 
 #Check if a File Exists
-#if
+read -p "provide path to check file exits or not" file_path
+
+if [ -e $file_path ]; then
+  echo "$file_path file exits"
+else
+  echo "$file_path does not exits"
+fi
