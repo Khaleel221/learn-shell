a908583ea4579cf16a7aeebbfdd2a07edbb15406
Khaleel <khaleelshaik711@gmail.com>
2025-10-03 07:41:04 +0530

    conditions in shell scripting

--- condtions.sh
+++ condtions.sh
@@ -30,11 +30,44 @@
 
 #Check User Login
 
-read -p "enter user name " user
+#read -p "enter user name " user
+#
+#if whoami | grep -q $user ; then
+#  echo "$user logged in"
+#else
+#  echo "$user not logged in"
+#fi
 
-if whoami | grep -q $user ; then
-  echo "$user logged in"
-else
-  echo "$user not logged in"
-  echo $?
-fi
+#case statement
+echo "enter 1 to create a file"
+echo "enter 2 create a directory"
+echo "enter 3 create user"
+echo "enter 4 install nginx"
+
+read -p "enter your choice " choice
+
+case $choice in
+1)
+  read -p "enter filename" filename
+  touch $filename
+  echo "file create successfully"
+  ;;
+2)
+  read -p "enter directory name" directory
+  mkdir $directory
+  echo "directory create successfully"
+  ;;
+3)
+  read -p "enter user name" user
+  useradd $user
+  echo "user create successfully"
+;;
+4)
+  read -p "enter package name" package
+  useradd $package
+  echo "package create successfully"
+;;
+*)
+  echo "invalid option"
+;;
+esac
