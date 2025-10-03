0d3aca193e1b817e10480ba7cd5dc24d24789392
Khaleel <khaleelshaik711@gmail.com>
2025-10-03 06:37:30 +0530

    conditions in shell scripting

---
+++ condtions.sh
@@ -0,0 +1,10 @@
+#Check if a Number is Positive or Negative
+
+a=10
+
+if [$a -gt 0]; then
+  echo "$a is postive number"
+  elif [$a -lt 0]; then
+    echo "$a is negative number"
+    else
+      echo "number is zero"
