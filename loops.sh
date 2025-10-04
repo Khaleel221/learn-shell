b179d18fea603a0bc82a497e97cb51340096a716
Khaleel <khaleelshaik711@gmail.com>
2025-10-04 12:21:49 +0530

    loops in shell scripting

--- loops.sh
+++ loops.sh
@@ -1,6 +1,11 @@
-number=1
-while [ $number -lt 10 ]
+#number=1
+#while [ $number -lt 10 ]
+#do
+#echo "number is: $number"
+#((number++))
+#done
+
+for ((number=1; $number<10; number++))
 do
-echo "number is: $number"
-((number++))
+  echo "number is $number"
 done
